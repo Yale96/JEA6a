6fa21d49472b2154b7c6706d0ed7f0173c2d3168
Yale96 <yannickvanleeuwen@i-lion.nl>
2018-02-06 08:01:37 +0100

    Setup

---
+++ Rekenmachine/public_html/calc.js
@@ -0,0 +1,43 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function c(val) {
+    document.getElementById("display").value = val;
+}
+function math(val) {
+    document.getElementById("display").value += val;
+}
+function e()
+{
+    try
+    {
+        c(eval(document.getElementById("display").value))
+    } catch (e)
+    {
+        c('Error')
+    }
+}
+
+function loadDocBig(url) {
+    var head = document.getElementsByTagName('head')[0],
+            link = document.createElement('link');
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.href = url;
+    head.appendChild(link);
+    return link;
+}
+
+function loadDocSmall(url) {
+    var head = document.getElementsByTagName('head')[0],
+            link = document.createElement('link');
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.href = url;
+    head.appendChild(link);
+    return link;
+}
+
